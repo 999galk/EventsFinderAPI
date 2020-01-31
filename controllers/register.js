c6a98b584b1b18bcd96b5047c3654020980d5fc8
Gal Kol <999galk@gmail.com>
2020-01-31 21:06:26 +0200

    added logs

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,7 @@
 const handleRegister = (req,res,db,bcrypt,method, request) => {
 	const { email, name , password} = req.body;
 	console.log('got to register.js, details to db:', email, name , password);
+	console.log('db:',db);
 	if( !email || !name || !password ){
 		return res.status(400).json('incorrect form submission');
 	}
