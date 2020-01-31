e018f19f1bb4c92defc74d4a17b3d5d01f3a040e
Gal Kol <999galk@gmail.com>
2020-01-31 22:05:37 +0200

    ssl true,

--- controllers/register.js
+++ controllers/register.js
@@ -5,34 +5,34 @@ const handleRegister = (req,res,db,bcrypt,method, request) => {
 		return res.status(400).json('incorrect form submission');
 	}
 	const hash = bcrypt.hashSync(password);
-	db.select('*').from('login').where({
-		id : 1}).then(user => {
-		if(user.length){
-			console.log(user[0]);
-		} else{
-			res.status(404).json('user doesnt exist');
-		}
-	}).catch(err => res.status(400).json('Error getting users'));
+	// db.select('*').from('login').where({
+	// 	id : 1}).then(user => {
+	// 	if(user.length){
+	// 		console.log(user[0]);
+	// 	} else{
+	// 		res.status(404).json('user doesnt exist');
+	// 	}
+	// }).catch(err => res.status(400).json('Error getting users'));
 
 
-	// db.transaction(trx =>{
-	// 	trx.insert({
-	// 		hash : hash,
-	// 		email : email
-	// 	}).into('login').returning('email').then(loginEmail => {
-	// 		return trx('users')
-	// 		.returning('*')
-	// 		.insert({
-	// 			name: name,
-	// 			email: loginEmail[0],
-	// 			joined : new Date()
-	// 		}).then(user => {
-	// 			res.status(200).json((user[0]).toString());
-	// 		}).catch(err => console.log('error writing to DB:', err))
-	// 	})
-	// 	.then(trx.commit)
-	// 	.catch(trx.rollback)
-	// }).catch(err => res.status(400).json('error',err));
+	db.transaction(trx =>{
+		trx.insert({
+			hash : hash,
+			email : email
+		}).into('login').returning('email').then(loginEmail => {
+			return trx('users')
+			.returning('*')
+			.insert({
+				name: name,
+				email: loginEmail[0],
+				joined : new Date()
+			}).then(user => {
+				res.status(200).json((user[0]).toString());
+			}).catch(err => console.log('error writing to DB:', err))
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	}).catch(err => res.status(400).json('error',err));
 
 }
 
